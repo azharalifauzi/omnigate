21733f37ce697fa6fffa42e2f196b8d553131c51
Azhar Ali Fauzi <azharalifauzi@gmail.com>
2024-12-01 16:12:23 +0700

    feat: add empty state

--- apps/web/app/admin/organizations/[id]/feature-flags/client.tsx
+++ apps/web/app/admin/organizations/[id]/feature-flags/client.tsx
@@ -7,6 +7,9 @@ import { client, QueryKey, unwrapResponse } from '~/utils/fetcher'
 import OrganizationDetailLayout from '../_components/detail-layout'
 import FeatureFlagTable from './_components/table'
 import { useParams } from 'next/navigation'
+import { match } from 'ts-pattern'
+import { Button } from '@repo/ui/components/ui/button'
+import Link from '~/components/link'
 
 const FeatureFlagsFeature = () => {
   const [page, setPage] = useState(1)
@@ -40,13 +43,33 @@ const FeatureFlagsFeature = () => {
           <h1 className="text-3xl font-medium">Feature Flags</h1>
         </div>
         <div className="mt-8">
-          <FeatureFlagTable data={data?.data || []} />
-          <Pagination
-            className="justify-end mt-4"
-            totalPages={data?.pageCount ?? 1}
-            currentPage={page}
-            onPageChange={setPage}
-          />
+          {match(data?.data.length ?? 0)
+            .with(0, () => (
+              <div className="flex flex-col items-center text-center max-w-lg mx-auto mt-24">
+                <div className="font-medium mb-4 text-2xl">
+                  Manage Feature Flags
+                </div>
+                <div className="mb-4">
+                  At the moment, there are no feature flags available for this
+                  organization. You&apos;ll need to configure them for your
+                  business first.
+                </div>
+                <Button asChild>
+                  <Link href="/admin/feature-flags">Add Feature Flag</Link>
+                </Button>
+              </div>
+            ))
+            .otherwise(() => (
+              <>
+                <FeatureFlagTable data={data?.data || []} />
+                <Pagination
+                  className="justify-end mt-4"
+                  totalPages={data?.pageCount ?? 1}
+                  currentPage={page}
+                  onPageChange={setPage}
+                />
+              </>
+            ))}
         </div>
       </div>
     </OrganizationDetailLayout>
